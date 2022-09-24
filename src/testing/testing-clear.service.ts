f8f6c2b95cf3ade8a9681268a62c4aa98b962713
izacode <izaelec@gmail.com>
2022-09-24 10:07:19 -0400

    testing module to sql, type orm settings in app.module changed

--- src/testing/testing-clear.service.ts
+++ src/testing/testing-clear.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
+import { InjectDataSource } from '@nestjs/typeorm';
 import { Model } from 'mongoose';
+import { DataSource } from 'typeorm';
 import { Attempt } from '../schemas/attempt.schema';
 import { Blogger } from '../schemas/blogger.schema';
 import { CommentReaction } from '../schemas/comment-reaction.schema';
@@ -11,51 +13,64 @@ import { User } from '../schemas/user.schema';
 
 @Injectable()
 export class TestingClearService {
-  @InjectModel(Post.name) private postModel: Model<Post>;
-  @InjectModel(Blogger.name) private bloggerModel: Model<Blogger>;
-  @InjectModel(Comment.name) private commentModel: Model<Comment>;
-  @InjectModel(Attempt.name) private attemptModel: Model<Attempt>;
-  @InjectModel(PostReaction.name)
-  private postReactionModel: Model<PostReaction>;
-  @InjectModel(CommentReaction.name)
-  private commentReactionModel: Model<CommentReaction>;
-  @InjectModel(User.name) private userModel: Model<User>;
+  constructor(@InjectDataSource() protected dataSource: DataSource) {}
   async dropTestBase() {
     try {
-      await this.bloggerModel.collection.drop();
+      await this.dataSource.query(`
+      DELETE FROM public.bloggers
+	    WHERE true;
+      `);
     } catch {
       console.log('Bloggers collection does not exist');
     }
     try {
-      await this.postModel.collection.drop();
+      await this.dataSource.query(`
+      DELETE FROM public."Posts"
+	    WHERE true;
+      `);
     } catch {
       console.log('Posts collection does not exist');
     }
     try {
-      await this.commentModel.collection.drop();
+      await this.dataSource.query(`
+      DELETE FROM public."Comments"
+	    WHERE true;
+      `);
     } catch {
       console.log('Comments collection does not exist');
     }
     try {
-      await this.postReactionModel.collection.drop();
+      await this.dataSource.query(`
+      DELETE FROM public."PostReactions"
+	    WHERE true;
+      `);
     } catch {
       console.log('PostReactions collection does not exist');
     }
     try {
-      await this.attemptModel.collection.drop();
+      await this.dataSource.query(`
+      DELETE FROM public."Attempts"
+	    WHERE true;
+      `);
     } catch {
       console.log('Attempts collection does not exist');
     }
     try {
-      await this.commentReactionModel.collection.drop();
+      await this.dataSource.query(`
+      DELETE FROM public."CommentReactions"
+	    WHERE true;
+      `);
     } catch {
       console.log('CommentReactions collection does not exist');
     }
-    try {
-      await this.userModel.collection.drop();
-    } catch {
-      console.log('Users collection does not exist');
-    }
+     try {
+       await this.dataSource.query(`
+      DELETE FROM public."Users"
+	    WHERE true;
+      `);
+     } catch {
+       console.log('Users collection does not exist');
+     }
     return;
   }
 }
